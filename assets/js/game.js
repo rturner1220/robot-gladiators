fc737888405a1b12b537e04dc5f6869241e0e46b
Rosa Turner <rmturner1220@gmail.com>
2022-05-09 21:21:40 -0400

    javascript test

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
 window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+var playerName = window.prompt("What is your robot's name?");
